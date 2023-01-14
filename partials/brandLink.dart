273d7982698d756226ddb8c86b738833276cbb70
PiotrZPL <zpl20052020@protonmail.ch>
2023-01-14 15:11:43 +0100

    Added dark mode switch button size changing

--- partials/brandLink.dart
+++ partials/brandLink.dart
@@ -8,16 +8,20 @@ class BrandLink extends HtmlWidget {
     id,
     title,
     required this.brand,
-    this.link
+    this.link,
+    this.width = 50,
+    this.height = 40,
   }) : super(widget_class: widget_class, style: style, id: id, title: title);
 
   String brand;
   String? link;
+  int width = 50;
+  int height = 40;
 
   @override
   String toHTML() {
     String brandsvg = File("brands/$brand.svg").readAsStringSync();
-    brandsvg = brandsvg.replaceFirst("svg ", """svg class="fill-current" width="50" height="40" """);
+    brandsvg = brandsvg.replaceFirst("svg ", """svg class="fill-current" width="$width" height="$height" """);
     return Hyperlink(
       widget_class: widget_class,
       href: link,
